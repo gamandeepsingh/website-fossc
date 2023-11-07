7ba320e144e3da0da971cb4750260fcbb366b1fd
vivekpal1 <vivekpal@duck.com>
2023-11-07 07:07:08 +0530

    example schema

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello via Bun!");
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const newUser = await prisma.user.create({
+    data: {
+      email: 'someone@example.com',
+      name: 'Someone',
+      role: 'USER',
+    },
+  });
+  console.log('Created new user: ', newUser);
+  const allPosts = await prisma.post.findMany();
+  console.log('All posts: ', allPosts);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+export { prisma };
