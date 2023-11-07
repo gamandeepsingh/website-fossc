907e698ff01825fb6eae0a0fbe72157568ccc6ee
vivekpal1 <vivekpal@duck.com>
2023-11-07 07:15:09 +0530

    scripts module

--- scripts/run-workspaces.ts
+++ scripts/run-workspaces.ts
@@ -1,16 +1,16 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-const path = require('path');
-const glob = require('glob');
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+import glob from 'glob';
 
-const workspaces = ['packages/*', 'apps/*', 'configs/*'];
+const workspaces = ['packages/*', 'apps/*', 'configs/*', 'scripts/*'];
 
 const command = process.argv[2];
 
 workspaces.forEach((workspacePattern) => {
   const workspaceDirs = glob.sync(workspacePattern);
 
-  workspaceDirs.forEach((workspaceDir) => {
+  workspaceDirs.forEach((workspaceDir: any) => {
     const packageJsonPath = path.join(workspaceDir, 'package.json');
     if (fs.existsSync(packageJsonPath)) {
       const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
